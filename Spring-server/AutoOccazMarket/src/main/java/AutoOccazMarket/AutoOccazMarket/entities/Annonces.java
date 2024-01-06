c477ed1c712b1053ee3240b953b8ad5220fdf50f
DylanEverest <ratianarivodylan@gmail.com>
2024-01-06 16:36:29 +0300

    annonces

---
+++ Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/entities/Annonces.java
@@ -0,0 +1,81 @@
+package AutoOccazMarket.AutoOccazMarket.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Annonces {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idAnnonce ;
+
+    @ManyToOne(fetch = FetchType.EAGER ,optional = false)
+    private Modeles modeles ;
+
+    @ManyToOne(fetch = FetchType.EAGER ,optional = false)
+    private Utilisateur utilisateur;
+
+    @Column(name = "etat_general")
+    private Integer etatGeneral;
+
+    private String localisation ;
+
+    @Column(name = "etat_validation",nullable = false)
+    private Integer etatValidation;
+
+
+    public Integer getIdAnnonce() {
+        return idAnnonce;
+    }
+
+    public void setIdAnnonce(Integer idAnnonce) {
+        this.idAnnonce = idAnnonce;
+    }
+
+    public Modeles getModeles() {
+        return modeles;
+    }
+
+    public void setModeles(Modeles modeles) {
+        this.modeles = modeles;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
+
+    public Integer getEtatGeneral() {
+        return etatGeneral;
+    }
+
+    public void setEtatGeneral(Integer etatGeneral) {
+        this.etatGeneral = etatGeneral;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public void setLocalisation(String localisation) {
+        this.localisation = localisation;
+    }
+
+    public Integer getEtatValidation() {
+        return etatValidation;
+    }
+
+    public void setEtatValidation(Integer etatValidation) {
+        this.etatValidation = etatValidation;
+    }
+    
+}
