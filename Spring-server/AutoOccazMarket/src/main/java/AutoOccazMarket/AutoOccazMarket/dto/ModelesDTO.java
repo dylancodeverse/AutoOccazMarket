48af4961d6eea4502582844cb390df35fc7e81a0
DylanEverest <ratianarivodylan@gmail.com>
2024-01-06 16:46:09 +0300

    tay

---
+++ Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/dto/ModelesDTO.java
@@ -0,0 +1,52 @@
+package AutoOccazMarket.AutoOccazMarket.dto;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import AutoOccazMarket.AutoOccazMarket.entities.Modeles;
+
+
+
+@Component
+public class ModelesDTO 
+{
+    @Autowired
+    Modeles [] listModeles ;    
+
+    Modeles modeles ;
+
+    String errors ;
+
+    public void setModelesAsList(List<Modeles> list)
+    {
+        listModeles = list.toArray(new Modeles[list.size()]);
+    }
+
+    public Modeles[] getListModeles() {
+        return listModeles;
+    }
+
+    public void setListModeles(Modeles[] listModeles) {
+        this.listModeles = listModeles;
+    }
+
+    public Modeles getModeles() {
+        return modeles;
+    }
+
+    public void setModeles(Modeles modeles) {
+        this.modeles = modeles;
+    }
+
+    public String getErrors() {
+        return errors;
+    }
+
+    public void setErrors(String errors) {
+        this.errors = errors;
+    }
+
+}
+
