12ca6cd197710e42eb57abf1bea9a76716080796
DylanEverest <ratianarivodylan@gmail.com>
2024-01-06 15:56:51 +0300

    inona

---
+++ Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/repositories/MarqueRepository.java
@@ -0,0 +1,9 @@
+package AutoOccazMarket.AutoOccazMarket.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import AutoOccazMarket.AutoOccazMarket.entities.Marque;
+
+public interface MarqueRepository extends JpaRepository <Marque,Integer>{
+    
+}
