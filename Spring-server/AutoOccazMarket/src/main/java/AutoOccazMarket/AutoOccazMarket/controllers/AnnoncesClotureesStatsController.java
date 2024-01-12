14011913da21b26b02655ac8801e5041874b81f3
DylanEverest <ratianarivodylan@gmail.com>
2024-01-12 12:02:49 +0300

    end backOffice

--- Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/controllers/AnnoncesClotureesStatsController.java
+++ Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/controllers/AnnoncesClotureesStatsController.java
@@ -6,18 +6,29 @@ import AutoOccazMarket.AutoOccazMarket.dto.AnnoncesClotureesStatsDTO;
 import AutoOccazMarket.AutoOccazMarket.entities.AnnoncesClotureesStats;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class AnnoncesClotureesStatsController {
 
+    @Value("${spring.datasource.url}")
+    private String url ;
+
+    @Value("${spring.datasource.username}")
+    private String username ;
+
+    @Value("${spring.datasource.password}")
+    private String password ;
+
     @GetMapping("/AnnoncesClotureesStats")
     public AnnoncesClotureesStatsDTO select() {
         AnnoncesClotureesStatsDTO c = new AnnoncesClotureesStatsDTO();
         try {
-            Connection con = null ;
+            Connection con = DriverManager.getConnection(url, username, password) ;
             c.setListAnnoncesClotureesStats(AnnoncesClotureesStats.select(con));            
         } catch (Exception e) {
             c.setErrors(e.getMessage());            
--- Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/controllers/ModelesStatsController.java
+++ Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/controllers/ModelesStatsController.java
@@ -6,19 +6,31 @@ import AutoOccazMarket.AutoOccazMarket.dto.ModelesStatsDTO;
 import AutoOccazMarket.AutoOccazMarket.entities.ModelesStats;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class ModelesStatsController 
 {
+    @Value("${spring.datasource.url}")
+    private String url ;
+
+    @Value("${spring.datasource.username}")
+    private String username ;
+
+    @Value("${spring.datasource.password}")
+    private String password ;
     @GetMapping("/modelesStatsController")
+    
+    
     public ModelesStatsDTO getAll() {
         ModelesStatsDTO m = new ModelesStatsDTO();
         try {
-            Connection connection =null;
-            m.setListModelesStats( ModelesStats.select(connection));
+            Connection con = DriverManager.getConnection(url, username, password) ;
+            m.setListModelesStats( ModelesStats.select(con));
         } catch (Exception e) {
             m.setErrors(e.getMessage());
         }
