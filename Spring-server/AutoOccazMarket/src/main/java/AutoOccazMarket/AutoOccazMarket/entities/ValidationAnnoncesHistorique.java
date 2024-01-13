bdd7a5dcfa45ea205a0347c7b752119b1ffa002b
DylanEverest <ratianarivodylan@gmail.com>
2024-01-13 15:53:35 +0300

    validation

--- Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/entities/ValidationAnnoncesHistorique.java
+++ Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/entities/ValidationAnnoncesHistorique.java
@@ -4,7 +4,6 @@ import java.sql.Timestamp;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
