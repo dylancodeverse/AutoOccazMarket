5b46a6c45dbbf0a95f077ed06b48cfea942ca884
DylanEverest <ratianarivodylan@gmail.com>
2024-01-13 20:21:22 +0300

    ok backoffice no security

--- Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/entities/AnnoncesClotureesStats.java
+++ Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/entities/AnnoncesClotureesStats.java
@@ -15,7 +15,7 @@ public class AnnoncesClotureesStats {
 
     public static AnnoncesClotureesStats[] select(Connection connection ) throws Exception{
         Statement statement = connection.createStatement();
-        ResultSet re = statement.executeQuery("with V_modeles_annonces as (select id_modeles , nom_modele , id_annonce , etat_general , etat_validation , localisation , modeles_id_modeles , utilisateur_id_utilisateur , prix , id_annonce from modeles join annonces  on modeles_id_modeles = id_modeles and etat_validation= 30  ) , V_annonces_commission as (select V_modeles_annonces.* , commission.pourcentages from V_modeles_annonces join commission on V_modeles_annonces.id_modeles = commission.annonces_id_annonce ) select nom_modele , count(*) as nbreVoitureVendu, sum (prix*pourcentages) as prixvendu from V_annonces_commission group by nom_modele");
+        ResultSet re = statement.executeQuery("WITH V_modeles_annonces AS (SELECT modeles.id_modeles, modeles.nom_modele, annonces.id_annonce, annonces.etat_general, annonces.etat_validation, annonces.localisation, annonces.modeles_id_modeles, annonces.utilisateur_id_utilisateur, annonces.prix FROM modeles JOIN annonces ON modeles.id_modeles = annonces.modeles_id_modeles WHERE annonces.etat_validation = 30), V_annonces_commission AS (SELECT V_modeles_annonces.*, commission.pourcentages FROM V_modeles_annonces JOIN commission ON V_modeles_annonces.id_annonce = commission.annonces_id_annonce) SELECT nom_modele, COUNT(*) AS nbreVoitureVendu, SUM(prix * pourcentages) AS prixvendu FROM V_annonces_commission GROUP BY nom_modele");
         ArrayList<AnnoncesClotureesStats> l = new ArrayList<>();
         while (re.next()) {
             AnnoncesClotureesStats v = new AnnoncesClotureesStats();
