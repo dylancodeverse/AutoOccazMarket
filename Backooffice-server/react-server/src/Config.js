2b06f42e472b1a3ef83edbeccad3605f5a0d0bf0
DylanEverest <ratianarivodylan@gmail.com>
2024-01-15 14:00:17 +0300

    modified:   Backooffice-server/react-server/src/Config.js

--- Backooffice-server/react-server/src/Config.js
+++ Backooffice-server/react-server/src/Config.js
@@ -1,6 +1,5 @@
 // config.js
 const API_BASE_URL = 'http://localhost:8080';
 
-export const secretKey = '3be7a505483c0050243c5cbad4700da13925aa4137a55e9e33efd8bc4d05850f3be7a505483c0050243c5cbad4700da13925aa4137a55e9e33efd8bc4d05850f'
 
 export default API_BASE_URL;
