ed268d4a6ed25dc486d66608435f6d11f1a3277a
DylanEverest <ratianarivodylan@gmail.com>
2024-01-29 07:24:36 +0300

    sort method

--- Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/entities/AnnoncesClotureesStats.java
+++ Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/entities/AnnoncesClotureesStats.java
@@ -28,10 +28,10 @@ public class AnnoncesClotureesStats {
         return l.toArray(new AnnoncesClotureesStats[l.size()]);
     }
 
-    public static AnnoncesClotureesStats[] SortByNomModele(Connection connection) throws Exception {
+    public static AnnoncesClotureesStats[] sortBy(Connection connection , String by ,String order) throws Exception {
         Statement statement = connection.createStatement();
         ResultSet re = statement.executeQuery(
-                "WITH V_modeles_annonces AS (SELECT modeles.id_modeles, modeles.nom_modele, annonces.id_annonce, annonces.etat_general, annonces.etat_validation, annonces.localisation, annonces.modeles_id_modeles, annonces.utilisateur_id_utilisateur, annonces.prix FROM modeles JOIN annonces ON modeles.id_modeles = annonces.modeles_id_modeles WHERE annonces.etat_validation = 30), V_annonces_commission AS (SELECT V_modeles_annonces.*, commission.pourcentages FROM V_modeles_annonces JOIN commission ON V_modeles_annonces.id_annonce = commission.annonces_id_annonce) SELECT nom_modele, COUNT(*) AS nbreVoitureVendu, SUM(prix - (prix * pourcentages/100)) AS prixvendu FROM V_annonces_commission GROUP BY nom_modele, ORDER BY nom_modele");
+                "WITH V_modeles_annonces AS (SELECT modeles.id_modeles, modeles.nom_modele, annonces.id_annonce, annonces.etat_general, annonces.etat_validation, annonces.localisation, annonces.modeles_id_modeles, annonces.utilisateur_id_utilisateur, annonces.prix FROM modeles JOIN annonces ON modeles.id_modeles = annonces.modeles_id_modeles WHERE annonces.etat_validation = 30), V_annonces_commission AS (SELECT V_modeles_annonces.*, commission.pourcentages FROM V_modeles_annonces JOIN commission ON V_modeles_annonces.id_annonce = commission.annonces_id_annonce) , v_end as (SELECT nom_modele, COUNT(*) AS nbreVoitureVendu, SUM(prix - (prix * pourcentages/100)) AS prixvendu FROM V_annonces_commission GROUP BY nom_modele), v_end2 as (select * from v_end union all  select nom_modele , 0 as nbrevoiturevendu , 0 prixvendu from modeles)select nom_modele , sum(nbrevoiturevendu) ,sum(prixvendu) from  v_end2 group by nom_modele ORDER BY "+by+" "+order);
         ArrayList<AnnoncesClotureesStats> l = new ArrayList<>();
         while (re.next()) {
             AnnoncesClotureesStats v = new AnnoncesClotureesStats();
@@ -41,37 +41,8 @@ public class AnnoncesClotureesStats {
             l.add(v);
         }
         return l.toArray(new AnnoncesClotureesStats[l.size()]);
-    }
+    }    
 
-    public static AnnoncesClotureesStats[] SortByNombreVoiture(Connection connection) throws Exception {
-        Statement statement = connection.createStatement();
-        ResultSet re = statement.executeQuery(
-                "WITH V_modeles_annonces AS (SELECT modeles.id_modeles, modeles.nom_modele, annonces.id_annonce, annonces.etat_general, annonces.etat_validation, annonces.localisation, annonces.modeles_id_modeles, annonces.utilisateur_id_utilisateur, annonces.prix FROM modeles JOIN annonces ON modeles.id_modeles = annonces.modeles_id_modeles WHERE annonces.etat_validation = 30), V_annonces_commission AS (SELECT V_modeles_annonces.*, commission.pourcentages FROM V_modeles_annonces JOIN commission ON V_modeles_annonces.id_annonce = commission.annonces_id_annonce) SELECT nom_modele, COUNT(*) AS nbreVoitureVendu, SUM(prix - (prix * pourcentages/100)) AS prixvendu FROM V_annonces_commission GROUP BY nom_modele, ORDER BY nom_modele, ORDER BY nbrevoiturevendu");
-        ArrayList<AnnoncesClotureesStats> l = new ArrayList<>();
-        while (re.next()) {
-            AnnoncesClotureesStats v = new AnnoncesClotureesStats();
-            v.setNbrePosteClotures(re.getInt(2));
-            v.setNomModele(re.getString(1));
-            v.setPrixvendu(re.getDouble(3));
-            l.add(v);
-        }
-        return l.toArray(new AnnoncesClotureesStats[l.size()]);
-    }
-
-    public static AnnoncesClotureesStats[] SortByPrixVendu(Connection connection) throws Exception {
-        Statement statement = connection.createStatement();
-        ResultSet re = statement.executeQuery(
-                "WITH V_modeles_annonces AS (SELECT modeles.id_modeles, modeles.nom_modele, annonces.id_annonce, annonces.etat_general, annonces.etat_validation, annonces.localisation, annonces.modeles_id_modeles, annonces.utilisateur_id_utilisateur, annonces.prix FROM modeles JOIN annonces ON modeles.id_modeles = annonces.modeles_id_modeles WHERE annonces.etat_validation = 30), V_annonces_commission AS (SELECT V_modeles_annonces.*, commission.pourcentages FROM V_modeles_annonces JOIN commission ON V_modeles_annonces.id_annonce = commission.annonces_id_annonce) SELECT nom_modele, COUNT(*) AS nbreVoitureVendu, SUM(prix - (prix * pourcentages/100)) AS prixvendu FROM V_annonces_commission GROUP BY nom_modele, ORDER BY nom_modele, ORDER BY nbrevoiturevendu, ORDER BY prixvendu");
-        ArrayList<AnnoncesClotureesStats> l = new ArrayList<>();
-        while (re.next()) {
-            AnnoncesClotureesStats v = new AnnoncesClotureesStats();
-            v.setNbrePosteClotures(re.getInt(2));
-            v.setNomModele(re.getString(1));
-            v.setPrixvendu(re.getDouble(3));
-            l.add(v);
-        }
-        return l.toArray(new AnnoncesClotureesStats[l.size()]);
-    }
 
     public String getNomModele() {
         return nomModele;
