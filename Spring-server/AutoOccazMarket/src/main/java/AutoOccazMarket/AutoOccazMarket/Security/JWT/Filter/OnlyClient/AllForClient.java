753cf442b40e0d73b8c909b6e6b553861fbd79e6
DylanEverest <ratianarivodylan@gmail.com>
2024-01-29 23:16:06 +0300

    ok

--- Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/Security/JWT/Filter/OnlyClient/AllForClient.java
+++ Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/Security/JWT/Filter/OnlyClient/AllForClient.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,6 +16,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class AllForClient extends OncePerRequestFilter {
 
     @Autowired
