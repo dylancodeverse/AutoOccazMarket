e5fc552d03f8f94205e93fad42c5d8b5379aa699
Ratianarivo Ny Voara Hir' Ainatsara <111527495+dylancodeverse@users.noreply.github.com>
2024-01-29 10:45:38 +0300

    Delete Spring-server directory

--- Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/repositories/CommissionRepository.java
+++
@@ -1,9 +0,0 @@
-package AutoOccazMarket.AutoOccazMarket.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import AutoOccazMarket.AutoOccazMarket.entities.Commission;
-
-public interface CommissionRepository extends JpaRepository<Commission ,Integer>{
-    
-}
--- Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/repositories/PhotoAnnonceRepository.java
+++
@@ -1,9 +0,0 @@
-package AutoOccazMarket.AutoOccazMarket.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import AutoOccazMarket.AutoOccazMarket.entities.PhotoAnnonce;
-
-public interface PhotoAnnonceRepository extends JpaRepository <PhotoAnnonce,Integer>{
-    
-}
