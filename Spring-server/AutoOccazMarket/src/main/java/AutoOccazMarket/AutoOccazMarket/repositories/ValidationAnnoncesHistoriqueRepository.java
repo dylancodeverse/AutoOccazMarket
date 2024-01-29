7315b8acddd92d4175a9b9ae7bb901c9d8582827
Ratianarivo Ny Voara Hir' Ainatsara <111527495+dylancodeverse@users.noreply.github.com>
2024-01-29 21:35:36 +0300

    Delete Spring-server directory

--- Spring-server/AutoOccazMarket/src/main/java/AutoOccazMarket/AutoOccazMarket/repositories/ValidationAnnoncesHistoriqueRepository.java
+++
@@ -1,9 +0,0 @@
-package AutoOccazMarket.AutoOccazMarket.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import AutoOccazMarket.AutoOccazMarket.entities.ValidationAnnoncesHistorique;
-
-public interface ValidationAnnoncesHistoriqueRepository extends JpaRepository<ValidationAnnoncesHistorique,Integer>{
-    
-}
